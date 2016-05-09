309fd0706da51a453ea28efc4e694df6ae5e5999
dbetebenner <dbetebenner@gmail.com>
2016-05-09 08:52:11 -0400

    Adding prelim SGP script for PARCC data

---
+++ PARCC/PARCC_SGP_2016.R
@@ -0,0 +1,35 @@
+#########################################################################
+###
+### SGP analysis script for PARCC consortium
+###
+#########################################################################
+
+### Load Packages
+
+require(SGP)
+
+
+### Load Data
+
+
+
+
+### abcSGP
+
+PARCC_SGP <- abcSGP(
+		PARCC_Data_LONG,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "visualizeSGP", "outputSGP"),
+		sgp.percentiles=TRUE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		save.intermediate.results=TRUE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4)))
+
+
+### Save results
+
+save(PARCC_SGP, file="Data/PARCC_SGP.Rdata")
+
