0f9fc7fd16f071c35093dbd681940cdb333f43bd
Adam VanIwaarden <avaniwaarden@gmail.com>
2016-07-04 14:32:23 -0600

    Updates to data prep script based on PRELIM Spr '16 data

--- PARCC/PARCC_Data_LONG_2016.R
+++ PARCC/PARCC_Data_LONG_2016.R
@@ -33,24 +33,29 @@ read.parcc <- function(state, year, Fall=FALSE) {
 	my.zip <- grep(year, tmp.files, value=TRUE)
 	my.file <- gsub(".zip",  "", my.zip)
 	tmp.dir <- getwd()
-	setwd("~")
+	setwd(tempdir())
 	system(paste("unzip '", file.path(tmp.dir, tmp.name, "Data/Base_Files", my.zip), "'", sep=""))
-	TMP <- fread(my.file, sep=',', header=FALSE, skip=1L, colClasses=rep("character", 20))
+	if (year=="2014-2015" | Fall==TRUE) {
+		tmp.var.names <- head(all.var.names,-1)
+		cols <- 20
+	} else {
+		tmp.var.names <- all.var.names
+		cols <- 21
+	}
+	TMP <- fread(my.file, sep=',', header=FALSE, skip=1L, colClasses=rep("character", cols))
 	unlink(my.file)
 	setwd(tmp.dir)
-	if (year=="2015-2016" | Fall==TRUE) {
-		setnames(TMP, head(all.var.names,-1))
-	} else setnames(TMP, all.var.names)
+	setnames(TMP, tmp.var.names)
 	return(TMP)
 }
 
 ####  Spring 2016
 
 PARCC_Data_LONG_2016 <- rbindlist(list(
-	read.parcc("CO", "2015-2016"), read.parcc("IL", "2015-2016"), 
-	read.parcc("MD", "2015-2016"), read.parcc("MA", "2015-2016"), 
-	read.parcc("NJ", "2015-2016"), read.parcc("NM", "2015-2016"),
-	read.parcc("RI", "2015-2016"), read.parcc("DC", "2015-2016")))
+	read.parcc("CO", "D201606"), read.parcc("IL", "D201606"), 
+	read.parcc("MD", "D201606"), read.parcc("MA", "D201606"), 
+	read.parcc("NJ", "D201606"), read.parcc("NM", "D201606"),
+	read.parcc("RI", "D201606"), read.parcc("DC", "D201606")))
 
 setkey(PARCC_Data_LONG_2016, PARCCStudentIdentifier, TestCode, Period)
 PARCC_Data_LONG_2016 <- PARCC_Data_LONG_2016[, parcc.var.names, with=FALSE]
@@ -61,64 +66,73 @@ PARCC_Data_LONG_2016 <- PARCC_Data_LONG_2016[, parcc.var.names, with=FALSE]
 ###
 
 ####  ID
-setnames(PARCC_Data_LONG, "PARCCStudentIdentifier", "ID")
+setnames(PARCC_Data_LONG_2016, "PARCCStudentIdentifier", "ID")
 
 ####  CONTENT_AREA from TestCode
-PARCC_Data_LONG[, CONTENT_AREA := factor(TestCode)]
-levels(PARCC_Data_LONG$CONTENT_AREA) <- c("ALGEBRA_I", "ALGEBRA_II", rep("ELA", 9), "GEOMETRY", rep("MATHEMATICS", 6), "INTEGRATED_MATH_1", "INTEGRATED_MATH_2", "INTEGRATED_MATH_3")
-PARCC_Data_LONG[, CONTENT_AREA := as.character(CONTENT_AREA)]
+PARCC_Data_LONG_2016[, CONTENT_AREA := factor(TestCode)]
+levels(PARCC_Data_LONG_2016$CONTENT_AREA) <- c("ALGEBRA_I", "ALGEBRA_II", rep("ELA", 9), "GEOMETRY", rep("MATHEMATICS", 6), "INTEGRATED_MATH_1", "INTEGRATED_MATH_2", "INTEGRATED_MATH_3")
+PARCC_Data_LONG_2016[, CONTENT_AREA := as.character(CONTENT_AREA)]
 
 ####  GRADE from TestCode
-PARCC_Data_LONG[, GRADE := gsub("ELA|MAT", "", TestCode)]
-PARCC_Data_LONG[, GRADE := as.character(as.numeric(GRADE))]
-PARCC_Data_LONG[which(is.na(GRADE)), GRADE := "EOCT"]
-PARCC_Data_LONG[, GRADE := as.character(GRADE)]
+PARCC_Data_LONG_2016[, GRADE := gsub("ELA|MAT", "", TestCode)]
+PARCC_Data_LONG_2016[, GRADE := as.character(as.numeric(GRADE))]
+PARCC_Data_LONG_2016[which(is.na(GRADE)), GRADE := "EOCT"]
+PARCC_Data_LONG_2016[, GRADE := as.character(GRADE)]
 
 ####  YEAR
-PARCC_Data_LONG[, YEAR := gsub("-", "_", AssessmentYear)]
-PARCC_Data_LONG[which(Period == "FallBlock"), YEAR := paste(YEAR, "1", sep=".")]
-PARCC_Data_LONG[which(Period == "Spring"), YEAR := paste(YEAR, "2", sep=".")]
+PARCC_Data_LONG_2016[, YEAR := gsub("-", "_", AssessmentYear)]
+PARCC_Data_LONG_2016[which(Period == "FallBlock"), YEAR := paste(YEAR, "1", sep=".")]
+PARCC_Data_LONG_2016[which(Period == "Spring"), YEAR := paste(YEAR, "2", sep=".")]
 
 ####  Valid Cases
-PARCC_Data_LONG[, VALID_CASE := "VALID_CASE"]
+PARCC_Data_LONG_2016[, VALID_CASE := "VALID_CASE"]
 
 ####  Invalidate Cases with missing IDs
-PARCC_Data_LONG[which(is.na(ID)), VALID_CASE := "INVALID_CASE"]
+PARCC_Data_LONG_2016[which(is.na(ID)), VALID_CASE := "INVALID_CASE"]
 
 ####  Duplicates
-setkey(PARCC_Data_LONG, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID, SummativeScaleScore)
-setkey(PARCC_Data_LONG, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID)
-PARCC_Data_LONG[which(duplicated(PARCC_Data_LONG))-1, VALID_CASE := "INVALID_CASE"]
+setkey(PARCC_Data_LONG_2016, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID, SummativeScaleScore)
+setkey(PARCC_Data_LONG_2016, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID)
+PARCC_Data_LONG_2016[which(duplicated(PARCC_Data_LONG_2016))-1, VALID_CASE := "INVALID_CASE"]
 
 #  Duplicates if Grade ignored
-setkey(PARCC_Data_LONG, VALID_CASE, YEAR, CONTENT_AREA, ID, SummativeScaleScore)
-setkey(PARCC_Data_LONG, VALID_CASE, YEAR, CONTENT_AREA, ID)
-PARCC_Data_LONG[which(duplicated(PARCC_Data_LONG))-1, VALID_CASE := "INVALID_CASE"]
+setkey(PARCC_Data_LONG_2016, VALID_CASE, YEAR, CONTENT_AREA, ID, SummativeScaleScore)
+setkey(PARCC_Data_LONG_2016, VALID_CASE, YEAR, CONTENT_AREA, ID)
+PARCC_Data_LONG_2016[which(duplicated(PARCC_Data_LONG_2016))-1, VALID_CASE := "INVALID_CASE"]
 
 
 ####  Establish seperate Theta and Scale Score long data sets
 
-PARCC_Data_LONG_SS <- copy(PARCC_Data_LONG)
+PARCC_Data_LONG_SS <- copy(PARCC_Data_LONG_2016)
 
 PARCC_Data_LONG_SS[, IRTTheta := NULL]
 PARCC_Data_LONG_SS[, CONTENT_AREA := paste(CONTENT_AREA, "SS", sep="_")]
 setnames(PARCC_Data_LONG_SS, c("SummativeScaleScore", "SummativeCSEM"), c("SCALE_SCORE", "SCALE_SCORE_CSEM"))
 
 ####  Theta data set - create IRT CSEM First
-scaling.constants <- as.data.table(read.csv("/media/Data/Dropbox (SGP)/SGP/PARCC/PARCC/Data/Base_Files/2015-2016 PARCC Scaling Constants.csv"))
+# scaling.constants <- as.data.table(read.csv("/media/Data/Dropbox (SGP)/SGP/PARCC/PARCC/Data/Base_Files/2015-2016 PARCC Scaling Constants.csv"))
+scaling.constants <- as.data.table(read.csv("/media/Data/Dropbox (SGP)/SGP/PARCC/PARCC/Data/Base_Files/2014-2015 PARCC Scaling Constants.csv"))
 setkey(scaling.constants, CONTENT_AREA, GRADE)
-setkey(PARCC_Data_LONG, CONTENT_AREA, GRADE)
-PARCC_Data_LONG <- scaling.constants[PARCC_Data_LONG]
+setkey(PARCC_Data_LONG_2016, CONTENT_AREA, GRADE)
+PARCC_Data_LONG_2016 <- scaling.constants[PARCC_Data_LONG_2016]
 
-PARCC_Data_LONG[, SCALE_SCORE_CSEM := (as.numeric(SummativeCSEM))/a] # NO -b here...
+PARCC_Data_LONG_2016[, SCALE_SCORE_CSEM := (as.numeric(SummativeCSEM))/a] # NO -b here...
 
-PARCC_Data_LONG[, c("a", "b", "SummativeCSEM") := NULL]
+PARCC_Data_LONG_2016[, c("a", "b", "SummativeCSEM") := NULL]
 
-setnames(PARCC_Data_LONG, c("IRTTheta", "SummativeScaleScore"), c("SCALE_SCORE", "SCALE_SCORE_ACTUAL"))
+setnames(PARCC_Data_LONG_2016, c("IRTTheta", "SummativeScaleScore"), c("SCALE_SCORE", "SCALE_SCORE_ACTUAL"))
 
 ####  Stack Theta and SS Data
-PARCC_Data_LONG <- rbindlist(list(PARCC_Data_LONG, PARCC_Data_LONG_SS), fill=TRUE)
-PARCC_Data_LONG[, CONTENT_AREA := as.character(CONTENT_AREA)]
+PARCC_Data_LONG_2016 <- rbindlist(list(PARCC_Data_LONG_2016, PARCC_Data_LONG_SS), fill=TRUE)
+PARCC_Data_LONG_2016[, CONTENT_AREA := as.character(CONTENT_AREA)]
 
 ####  Save 2016 LONG Data
-save(PARCC_Data_LONG, file = "/media/Data/Dropbox (SGP)/SGP/PARCC/PARCC/Data/PARCC_Data_LONG_2016.Rdata")
+save(PARCC_Data_LONG_2016, file = "/media/Data/Dropbox (SGP)/SGP/PARCC/PARCC/Data/PARCC_Data_LONG_2016.Rdata")
+
+
+#####  Create SQLite Databases for each year / period
+require(RSQLite)
+file.create(parcc.db <- "/media/Data/Dropbox (SGP)/SGP/PARCC/PARCC/Data/PARCC_Data_LONG.sqlite")
+dbWriteTable(dbConnect(SQLite(), dbname = parcc.db), name = "PARCC_Data_LONG_2015_1", value=PARCC_Data_LONG[YEAR == "2014_2015.1"])
+dbWriteTable(dbConnect(SQLite(), dbname = parcc.db), name = "PARCC_Data_LONG_2015_2", value=PARCC_Data_LONG[YEAR == "2014_2015.2"])
+dbWriteTable(dbConnect(SQLite(), dbname = parcc.db), name = "PARCC_Data_LONG_2016_1", value=PARCC_Data_LONG[YEAR == "2015_2016.1"])
