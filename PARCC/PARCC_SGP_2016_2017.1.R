5e482aa48ec6d42e7d9332cf1a35c09f81c53752
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-02-14 09:15:38 -0500

    Change analysis script to use new archived data approach
    Now going to archive all SGP related objects and output for each
    assessment window.  No longer use updateSGP to update an object.  Now
    read in the previous @Data slot and add new data to it and run abcSGP.

--- PARCC/PARCC_SGP_2016_2017.1.R
+++ PARCC/PARCC_SGP_2016_2017.1.R
@@ -14,12 +14,10 @@ require(SGP)
 require(RSQLite)
 
 
-### Load Data
-if (sgp.test) {
-	load("./Data/SIM/PARCC_SGP-Test.Rdata")
-} else load("./Data/PARCC_SGP.Rdata")
+###  Load SGP LONG Data from Spring 2016 Analyses
+load("./Data/Archive/2016_2017.1/PARCC_SGP_LONG_Data.Rdata")
 
-# parcc.db <- "./Data/PARCC_Data_LONG_Simulated.sqlite"
+###  Location of PARCC SQLite Database (Fall 2016 added in Data Prep step)
 parcc.db <- "./Data/PARCC_Data_LONG.sqlite"
 
 
@@ -35,63 +33,44 @@ PARCC_2016_2017.1.config <- c(
 	ELA_2016_2017.1.config,
 	ELA_SS_2016_2017.1.config,
 
-	MATHEMATICS_2016_2017.1.config,
-	MATHEMATICS_SS_2016_2017.1.config,
-
 	ALGEBRA_I.2016_2017.1.config,
 	ALGEBRA_I_SS.2016_2017.1.config,
 	ALGEBRA_II.2016_2017.1.config,
 	ALGEBRA_II_SS.2016_2017.1.config,
 	GEOMETRY.2016_2017.1.config,
-	GEOMETRY_SS.2016_2017.1.config,
-
-	INTEGRATED_MATH_1.2016_2017.1.config,
-	INTEGRATED_MATH_1_SS.2016_2017.1.config,
-	INTEGRATED_MATH_2.2016_2017.1.config,
-	INTEGRATED_MATH_2_SS.2016_2017.1.config,
-	INTEGRATED_MATH_3.2016_2017.1.config,
-	INTEGRATED_MATH_3_SS.2016_2017.1.config
+	GEOMETRY_SS.2016_2017.1.config
 )
 
 
-### updateSGP
+### abcSGP
 
-PARCC_SGP <- updateSGP(
-		what_sgp_object=PARCC_SGP,
-		with_sgp_data_LONG = dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2017_1"),
+PARCC_SGP <- abcSGP(
+		state="PARCC",
+		sgp_object=rbindlist(list(
+			PARCC_SGP_LONG_Data,
+			dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2017_1"))),
 		sgp.config = PARCC_2016_2017.1.config,
-		steps=c("prepareSGP", "analyzeSGP"),
-		sgp.percentiles = TRUE,
-		sgp.projections = FALSE,
-		sgp.projections.lagged = FALSE,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+		prepareSGP.create.additional.variables=FALSE,
+		sgp.percentiles=TRUE,
+		sgp.projections=FALSE,
+		sgp.projections.lagged=FALSE,
 		sgp.percentiles.baseline=FALSE,
-		sgp.projections.baseline = FALSE,
-		sgp.projections.lagged.baseline = FALSE,
-		sgp.percentiles.equated = FALSE,
-		simulate.sgps = TRUE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
 		calculate.simex= if (sgp.test) list(lambda=seq(0,2,0.5), simulation.iterations=10, csem.data.vnames="SCALE_SCORE_CSEM", extrapolation="linear", save.matrices=TRUE) else TRUE,
 		sgp.test.cohort.size= if (sgp.test) 1500 else NULL,     ####
 		return.sgp.test.results= if (sgp.test) TRUE else FALSE, ## -- Turn OFF these 3 for real analyses
 		goodness.of.fit.print= if (sgp.test) FALSE else TRUE,   ####
 		save.intermediate.results=FALSE,
-		outputSGP.output.type=c("LONG_Data", "LONG_FINAL_YEAR_Data"),
-		parallel.config=list(BACKEND="FOREACH", TYPE="doParallel", WORKERS=list(TAUS = workers, SIMEX = workers)))
-
-
-### combineSGP
-
-PARCC_SGP <- combineSGP(PARCC_SGP)
-
-
-### outputSGP
-
-outputSGP(PARCC_SGP, outputSGP.directory=if (sgp.test) "Data/SIM" else "Data")
+		outputSGP.output.type=c("LONG_FINAL_YEAR_Data"),
+    parallel.config=list(BACKEND="FOREACH", TYPE="doParallel", WORKERS=list(TAUS = workers, SIMEX=workers)))
 
 
 ### Save results
 
 if (sgp.test) {
-	save(PARCC_SGP, file="./Data/SIM/PARCC_SGP-Test.Rdata")
+	save(PARCC_SGP, file="./Data/SIM/PARCC_SGP-Test_PARCC_2016_2017.1.Rdata")
 } else save(PARCC_SGP, file="./Data/PARCC_SGP.Rdata")
 
 
