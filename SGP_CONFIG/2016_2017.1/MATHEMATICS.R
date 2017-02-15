a5433f9feac07365b6dd1d72268fbe9159dc1913
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-02-14 23:25:30 -0500

    Add in marginal/known small cohort configs to produce SGP_NOTE values

--- SGP_CONFIG/2016_2017.1/MATHEMATICS.R
+++ SGP_CONFIG/2016_2017.1/MATHEMATICS.R
@@ -8,7 +8,22 @@ ALGEBRA_I.2016_2017.1.config <- list(
 	ALGEBRA_I.2016_2017.1 = list(
 		sgp.content.areas=c("MATHEMATICS", "MATHEMATICS", "ALGEBRA_I"),
 		sgp.panel.years=c("2014_2015.2", "2015_2016.2", "2016_2017.1"),
-		sgp.grade.sequences=list(c("7", "8", "EOCT")), # ~9th grade
+		sgp.grade.sequences=list(c("7", "8", "EOCT")), # ~ 9th grade
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_I.2016_2017.1 = list(
+		sgp.content.areas=c("MATHEMATICS", "ALGEBRA_I"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("7", "EOCT")), # ~ 8th grade
+			sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_I.2016_2017.1 = list(
+		sgp.content.areas=c("GEOMETRY", "ALGEBRA_I"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_I.2016_2017.1 = list(
+		sgp.content.areas=c("INTEGRATED_MATH_2", "ALGEBRA_I"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
 		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
 	ALGEBRA_I.2016_2017.1 = list( #  Fall to Fall period analyses
 		sgp.content.areas=c("MATHEMATICS", "ALGEBRA_I"),
@@ -17,7 +32,6 @@ ALGEBRA_I.2016_2017.1.config <- list(
 		sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=c("GEOMETRY", "ALGEBRA_I", "ALGEBRA_II"),
 			YEAR=c("2015_2016.2", "2015_2016.2", "2015_2016.2"), GRADE=c("EOCT", "EOCT", "EOCT")),
 		sgp.projection.grade.sequences=list("NO_PROJECTIONS"))
-
 )
 
 GEOMETRY.2016_2017.1.config <- list(
@@ -25,6 +39,31 @@ GEOMETRY.2016_2017.1.config <- list(
 		sgp.content.areas=c("MATHEMATICS", "ALGEBRA_I", "GEOMETRY"),
 		sgp.panel.years=c("2014_2015.2", "2015_2016.2", "2016_2017.1"),
 		sgp.grade.sequences=list(c("8", "EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	GEOMETRY.2016_2017.1 = list(
+		sgp.content.areas=c("ALGEBRA_II", "GEOMETRY"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	GEOMETRY.2016_2017.1 = list(
+		sgp.content.areas=c("INTEGRATED_MATH_1", "GEOMETRY"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	GEOMETRY.2016_2017.1 = list(
+		sgp.content.areas=c("INTEGRATED_MATH_2", "GEOMETRY"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	GEOMETRY.2016_2017.1 = list(
+		sgp.content.areas=c("INTEGRATED_MATH_3", "GEOMETRY"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	GEOMETRY.2016_2017.1 = list(
+		sgp.content.areas=c("MATHEMATICS", "GEOMETRY"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("8", "EOCT")),
 		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
 	GEOMETRY.2016_2017.1 = list( #  Fall to Fall period analyses
 		sgp.content.areas=c("ALGEBRA_I", "GEOMETRY"),
@@ -49,6 +88,26 @@ ALGEBRA_II.2016_2017.1.config <- list(
 		sgp.grade.sequences=list(c("EOCT", "EOCT", "EOCT")),
 		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
 	ALGEBRA_II.2016_2017.1 = list(
+		sgp.content.areas=c("ALGEBRA_I", "ALGEBRA_II"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_II.2016_2017.1 = list(
+		sgp.content.areas=c("INTEGRATED_MATH_2", "ALGEBRA_II"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_II.2016_2017.1 = list(
+		sgp.content.areas=c("INTEGRATED_MATH_3", "ALGEBRA_II"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("EOCT", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_II.2016_2017.1 = list(
+		sgp.content.areas=c("MATHEMATICS", "ALGEBRA_II"),
+		sgp.panel.years=c("2015_2016.2", "2016_2017.1"),
+		sgp.grade.sequences=list(c("8", "EOCT")),
+		sgp.projection.grade.sequences=list("NO_PROJECTIONS")),
+	ALGEBRA_II.2016_2017.1 = list( #  Fall to Fall period analyses
 		sgp.content.areas=c("ALGEBRA_I", "ALGEBRA_II"),
 		sgp.panel.years=c("2015_2016.1", "2016_2017.1"),
 		sgp.grade.sequences=list(c("EOCT", "EOCT")),
