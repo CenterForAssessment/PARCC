86ea056dc894a18dbb88e04cbd7f713e6987c081
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-06-29 14:11:39 -0600

    Add Scale Score content area (*_SS) projections to PARCC analyses

--- SGP_CONFIG/2016_2017.2/ELA_SS.R
+++ SGP_CONFIG/2016_2017.2/ELA_SS.R
@@ -9,7 +9,7 @@ ELA_SS.2016_2017.2.config <- list(
 		sgp.content.areas=rep("ELA_SS", 3),
 		sgp.panel.years=c("2014_2015.2", "2015_2016.2", "2016_2017.2"),
 		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8"), c("7", "8", "9"), c("8", "9", "10"), c("9", "10", "11")),
-		sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 8)),
+		# sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 8)),
 		sgp.norm.group.preference=0L),
 
 	ELA_SS.2016_2017.2 = list(
--- SGP_CONFIG/2016_2017.2/MATHEMATICS_SS.R
+++ SGP_CONFIG/2016_2017.2/MATHEMATICS_SS.R
@@ -11,7 +11,7 @@ MATHEMATICS_SS.2016_2017.2.config <- list(
 		sgp.content.areas=rep("MATHEMATICS_SS", 3),
 		sgp.panel.years=c("2014_2015.2", "2015_2016.2", "2016_2017.2"),
 		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8")),
-		sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 5)),
+		# sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 5)),
 		sgp.norm.group.preference=1L)
 )
 
@@ -24,7 +24,7 @@ ALGEBRA_I_SS.2016_2017.2.config <- list(
 		sgp.grade.sequences=list(c("7", "8", "EOCT")),
 		# sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=rep(PARCC.Math.Subjects, each=2),
 		#   YEAR=c("2015_2016.1", "2016_2017.1"), GRADE="EOCT"),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # 9th grade - CANONICAL
+		# sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # 9th grade - CANONICAL
 		sgp.norm.group.preference=0L),
 	ALGEBRA_I_SS.2016_2017.2 = list(
 		sgp.content.areas=c("MATHEMATICS_SS", "MATHEMATICS_SS", "ALGEBRA_I_SS"),
@@ -88,7 +88,7 @@ GEOMETRY_SS.2016_2017.2.config <- list(
 		# sgp.exact.grade.progression=TRUE, #  NO exact - use to calculate "ALGEBRA_I_SS", "GEOMETRY_SS" matrices AND avoid duplicate projections (Thetas).
 		# sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=PARCC.Math.Subjects,
 		# 	YEAR="2016_2017.1", GRADE="EOCT"),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # 10th grade - CANONICAL
+		# sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # 10th grade - CANONICAL
 		sgp.norm.group.preference=0L),
 
 	GEOMETRY_SS.2016_2017.2 = list(
@@ -179,7 +179,7 @@ ALGEBRA_II_SS.2016_2017.2.config <- list(
 		sgp.grade.sequences=list(c("EOCT", "EOCT", "EOCT")),
 		# sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=PARCC.Math.Subjects,
 		# 	YEAR="2016_2017.1", GRADE="EOCT"),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # CANONICAL
+		# sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # CANONICAL
 		sgp.norm.group.preference=0L),
 
 	ALGEBRA_II_SS.2016_2017.2 = list( # Fall - Spring
@@ -261,7 +261,7 @@ INTEGRATED_MATH_1_SS.2016_2017.2.config <- list(
 		sgp.grade.sequences=list(c("7", "8", "EOCT")),
 		# sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=PARCC.Math.Subjects,
 		# 	YEAR="2016_2017.1", GRADE="EOCT"),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # 9th grade - CANONICAL
+		# sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # 9th grade - CANONICAL
 		sgp.norm.group.preference=0L),
 
 	INTEGRATED_MATH_1.2016_2017.2 = list(
@@ -310,7 +310,7 @@ INTEGRATED_MATH_2_SS.2016_2017.2.config <- list(
 		sgp.grade.sequences=list(c("EOCT", "EOCT")),
 		# sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=PARCC.Math.Subjects,
 		# 	YEAR="2016_2017.1", GRADE="EOCT"),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # CANONICAL
+		# sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # CANONICAL
 		sgp.norm.group.preference=0L),
 
 	INTEGRATED_MATH_2.2016_2017.2 = list( # Fall - Spring  --  <1000 :: Include for SGP_NOTE
@@ -373,7 +373,7 @@ INTEGRATED_MATH_3_SS.2016_2017.2.config <- list(
 		sgp.grade.sequences=list(c("EOCT", "EOCT")),
 		# sgp.exclude.sequences = data.table(VALID_CASE = "VALID_CASE", CONTENT_AREA=PARCC.Math.Subjects,
 		# 	YEAR="2016_2017.1", GRADE="EOCT"),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # CANONICAL
+		# sgp.projection.grade.sequences=list("NO_PROJECTIONS"), # CANONICAL
 		sgp.norm.group.preference=0L),
 
 	INTEGRATED_MATH_3.2016_2017.2 = list( # Fall - Spring  --  <1000 :: Include for SGP_NOTE
