943efe5e9b383ecd25fb7db5bd08299bf7aefd6d
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-06-30 11:45:13 -0600

    Add NJ script and new fetchPARCC function

---
+++ New_Jersey/PARCC_NJ_SGP_2016_2017.2.R
@@ -0,0 +1,143 @@
+#################################################################################
+###                                                                           ###
+###      SGP analysis script for PARCC New_Jersey - Spring 2016 Analyses      ###
+###                                                                           ###
+#################################################################################
+
+
+if (!exists("sgp.test")) sgp.test <- FALSE
+workers <- parallel::detectCores()/2
+
+### Load Packages
+
+require(SGP)
+require(RSQLite)
+require(data.table)
+
+###  fetchPARCC function
+
+rbindlist(list(
+	dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2015_2 where StateAbbreviation in ('NJ')"),
+	dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2016_1 where StateAbbreviation in ('NJ')"),
+	dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2016_2 where StateAbbreviation in ('NJ')"),
+	dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2017_1 where StateAbbreviation in ('NJ')")), fill=TRUE)
+
+### Load configurations & combine
+
+source("../SGP_CONFIG/2015_2016.2/ELA.R")
+source("../SGP_CONFIG/2015_2016.2/ELA_SS.R")
+source("../SGP_CONFIG/2015_2016.2/MATHEMATICS.R")
+source("../SGP_CONFIG/2015_2016.2/MATHEMATICS_SS.R")
+
+
+PARCC_2015_2016.2.config <- c(
+	ELA_2015_2016.2.config,
+	ELA_SS_2015_2016.2.config,
+
+	MATHEMATICS_2015_2016.config,
+	MATHEMATICS_SS_2015_2016.config,
+
+	ALGEBRA_I.2015_2016.config,
+	ALGEBRA_I_SS.2015_2016.config,
+	ALGEBRA_II.2015_2016.config,
+	ALGEBRA_II_SS.2015_2016.config,
+	GEOMETRY.2015_2016.config,
+	GEOMETRY_SS.2015_2016.config,
+
+	INTEGRATED_MATH_1.2015_2016.config,
+	INTEGRATED_MATH_1_SS.2015_2016.config,
+	INTEGRATED_MATH_2.2015_2016.config,
+	INTEGRATED_MATH_2_SS.2015_2016.config,
+	INTEGRATED_MATH_3.2015_2016.config,
+	INTEGRATED_MATH_3_SS.2015_2016.config
+)
+
+
+### prepareSGP with Data read in directly from SQLite database
+
+# parcc.db <- "../PARCC/Data/PARCC_Data_LONG_Simulated.sqlite"
+parcc.db <- "../PARCC/Data/PARCC_Data_LONG.sqlite"
+
+New_Jersey_SGP <- prepareSGP(
+	state = "NJ",
+	data = rbindlist(list(
+		dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2015_2 where StateAbbreviation in ('NJ')"),
+		dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2016_1 where StateAbbreviation in ('NJ')"),
+		dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), "select * from PARCC_Data_LONG_2016_2 where StateAbbreviation in ('NJ')"))),
+	create.additional.variables=FALSE)
+
+
+### analyzeSGP (for student growth percentiles)
+
+New_Jersey_SGP <- analyzeSGP(
+		New_Jersey_SGP,
+		sgp.config=PARCC_2015_2016.2.config,
+		sgp.percentiles=TRUE,
+		sgp.projections=FALSE,
+		sgp.projections.lagged=FALSE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		calculate.simex= if (sgp.test) list(lambda=seq(0,2,0.5), simulation.iterations=10, csem.data.vnames="SCALE_SCORE_CSEM", extrapolation="linear", save.matrices=TRUE) else TRUE,
+		sgp.test.cohort.size= if (sgp.test) 1500 else NULL,     #### )
+		return.sgp.test.results= if (sgp.test) TRUE else FALSE, ##   > -- Turn OFF these 3 for real analyses
+		goodness.of.fit.print= if (sgp.test) FALSE else TRUE,   #### )
+		get.cohort.data.info=TRUE,
+		parallel.config=list(BACKEND="FOREACH", TYPE="doParallel", WORKERS=list(TAUS = workers, SIMEX = workers)))
+
+
+### analyzeSGP (for student growth projections)
+
+New_Jersey_SGP <- analyzeSGP(
+		New_Jersey_SGP,
+		sgp.config=PARCC_2015_2016.2.config,
+		sgp.percentiles=FALSE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		goodness.of.fit.print=FALSE,
+		parallel.config = if (sgp.test) NULL else list(BACKEND="FOREACH", TYPE="doParallel", WORKERS=list(PROJECTIONS = workers, LAGGED_PROJECTIONS = workers)))
+
+
+### combineSGP
+
+New_Jersey_SGP <- combineSGP(
+		New_Jersey_SGP,
+		sgp.target.scale.scores=TRUE,
+		sgp.config=PARCC_2015_2016.2.config,
+		parallel.config = if (sgp.test) NULL else list(BACKEND="FOREACH", TYPE="doParallel", WORKERS=list(SGP_SCALE_SCORE_TARGETS = workers)))
+
+
+### Save results
+
+dir.create("Data")
+if (sgp.test) {
+	save(New_Jersey_SGP, file="Data/SIM/New_Jersey_SGP-Test.Rdata")
+} else save(New_Jersey_SGP, file="Data/New_Jersey_SGP.Rdata")
+
+
+### outputSGP
+
+outputSGP(New_Jersey_SGP, outputSGP.directory=if (sgp.test) "Data/SIM" else "Data")
+
+
+### visualizeSGP
+
+###  Need to modify the GRADE, CONTENT_AREA and Year Lag projection sequences to
+###  Accurately reflect the course taking patterns in the state (the
+###  original meta-data are based on the entire PARCC Consortium).
+
+table(New_Jersey_SGP@Data[YEAR=='2015_2016.2' & !is.na(SGP), CONTENT_AREA])
+table(New_Jersey_SGP@Data[YEAR=='2015_2016.2' & !is.na(SGP) & CONTENT_AREA=="ELA", GRADE])
+
+SGPstateData[["NJ"]][["Student_Report_Information"]][["Content_Areas_Domains"]] <- list(ELA="ELA", MATHEMATICS="MATHEMATICS", ALGEBRA_I="MATHEMATICS", GEOMETRY="MATHEMATICS", ALGEBRA_II="MATHEMATICS")
+
+visualizeSGP(
+	New_Jersey_SGP,
+	plot.types=c("growthAchievementPlot"),
+	# plot.types=c("growthAchievementPlot", "studentGrowthPlot"),
+	parallel.config=list(BACKEND="PARALLEL", WORKERS=list(GA_PLOTS=workers)))
+
+q("no")
---
+++ fetchPARCC.R
@@ -0,0 +1,48 @@
+###  fetchPARCC function
+fetchPARCC <- function(state=NULL, prior.years, current.year, fields="*") {
+
+		if (!is.null(state)) {
+			pull_priors <-
+				paste0("prior_data <- rbindlist(list(",
+					paste0("dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), \"select ", fields, " from ", paste0("PARCC_Data_LONG_", prior.years), " where StateAbbreviation in ('", state, "')\")", collapse=","), "), fill=TRUE)")
+			eval(parse(text=pull_priors))
+			###   INVALIDate duplicate prior test scores - keep highest THETA/SCALE_SCORE
+			setkey(prior_data, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID, SCALE_SCORE, SCALE_SCORE_ACTUAL)
+			setkey(prior_data, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID)
+			prior_data[which(duplicated(prior_data, by=key(prior_data)))-1, VALID_CASE := "INVALID_CASE"]
+
+			###  Current year
+			pull_current <- paste0("current_data <- data.table(dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), \"select ", fields, " from ", paste0("PARCC_Data_LONG_", current.year), " where StateAbbreviation in ('", state, "')\"))")
+			eval(parse(text=pull_current))
+			###   INVALIDate EXACT duplicate current test scores - create an indicator for subsequent data merge.
+			current_data[, EXACT_DUPLICATE := as.numeric(NA)]
+
+			setkey(current_data, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID, SCALE_SCORE_ACTUAL, SCALE_SCORE)
+			current_data[which(duplicated(current_data, by=key(current_data)))-1, EXACT_DUPLICATE := 1]
+			current_data[which(duplicated(current_data, by=key(current_data))), EXACT_DUPLICATE := 2]
+			current_data[which(duplicated(current_data, by=key(current_data))), VALID_CASE := "INVALID_CASE"]
+
+			tmp_data <- rbindlist(list(prior_data, current_data), fill=TRUE)
+			tmp_data <- SGP:::getAchievementLevel(tmp_data, state="PARCC")
+			return(tmp_data)
+		}
+
+		###  Else ...  PARCC consortium
+		pull_priors <-
+			paste0("prior_data <- rbindlist(list(",
+				paste0("dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), \"select ", fields, " from ", paste0("PARCC_Data_LONG_", prior.years), "\")", collapse=","), "), fill=TRUE)")
+		eval(parse(text=pull_priors))
+
+		###   INVALIDate duplicate prior test scores - keep highest THETA/SCALE_SCORE
+		setkey(prior_data, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID, SCALE_SCORE, SCALE_SCORE_ACTUAL)
+		setkey(prior_data, VALID_CASE, YEAR, CONTENT_AREA, GRADE, ID)
+		prior_data[which(duplicated(prior_data, by=key(prior_data)))-1, VALID_CASE := "INVALID_CASE"]
+
+		###  Current year
+		pull_current <- paste0("current_data <- dbGetQuery(dbConnect(SQLite(), dbname = parcc.db), \"select ", fields, " from ", paste0("PARCC_Data_LONG_", current.year), "\")")
+		eval(parse(text=pull_current))
+
+		tmp_data <- rbindlist(list(prior_data, current_data), fill=TRUE)
+		tmp_data <- SGP:::getAchievementLevel(tmp_data, state="PARCC")
+		return(tmp_data)
+}
